bebeb44f493a559a2b9f9ac10d24d0a8956bfed8
Pranesh M <praneshmunusami18@gmail.com>
2020-08-12 14:40:33 +0530

    Heroku Server Supported

--- public/Javascript/app.js
+++ public/Javascript/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  const url = "hah/weather?address=" + location;
+  const url = "/weather?address=" + location;
   fetch(url).then((response) => {
     response.json().then((data) => {
       if (data.error) {
